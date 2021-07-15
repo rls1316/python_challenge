ccb80001f2051abf5bb9463fadc25744495df149
rls1316 <80739270+rls1316@users.noreply.github.com>
2021-07-14 23:29:32 -0700

    saving point, revising print still

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,7 +10,6 @@ total_votes = 0
 candidate_vote_count = {}
 candidate_vote_percentage = {}
 winning_count = 0
-
 # Open the CSV and bypass the header
 with open(csv_path, newline = '') as csv_file:
     csvreader = csv.reader(csv_file, delimiter = ",")
@@ -21,21 +20,21 @@ with open(csv_path, newline = '') as csv_file:
         total_votes += 1
         if row[2] in candidate_vote_count:
             candidate_vote_count[row[2]] += 1
-        else candidate_vote_count[row[2]] = 1
+        else: candidate_vote_count[row[2]] = 1
 
 # Loop through candididate_vote_count dictionary to identify each candidates % and determine a winner
-    for candidate in candidate_vote_count:
-        candidate_vote_percentage[candidate] = format((candidate_vote_count[candidate]/total_votes) * 100, '.3f')
-        if candidate_vote_count > winning count:
-            winning_count = candidate_vote_count[candidate]
-            winner = candidate
+for candidate in candidate_vote_count:
+    candidate_vote_percentage[candidate] = format((candidate_vote_count[candidate]/total_votes) * 100, '.3f')
+    if candidate_vote_count[candidate] > winning_count:
+        winning_count = candidate_vote_count[candidate]
+        winner = candidate
 
 # Print Results
 Election_Results = (f'''Election Results
 -------------------------
 Total Votes: {total_votes}
 -------------------------
-{candidate}: {candidate_vote_percentage[candidate]}% ({candiate_vote_count[candidate]})
+{candidate}: {candidate_vote_percentage[candidate]}% ({candidate_vote_count[candidate]})
 -------------------------
 Winner: {winner}
 -------------------------''')
